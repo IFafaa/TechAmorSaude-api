fd57bfc028360a65f9981b639c6f1ad1442113f2
Fabricio Lima <fabriciotrab2005@gmail.com>
2024-02-21 22:20:29 -0300

    feat: adicionando imports a app module e ajustando middleware para rotas especificas ( dos novos cruds criados)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { DatabaseModule } from "./database/database.module";
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from "@nestjs/common";
 import { AuthModule } from "./modules/auth/auth.module";
 import { AuthMiddleware } from "./common/middlewares/auth.middleware";
 import { UserModule } from "./modules/user/user.module";
+import { RegionModule } from "./modules/region/region.module";
+import { MedicalSpecialtyModule } from "./modules/medical_specialty/medical_specialty.module";
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UserModule],
+  imports: [
+    DatabaseModule,
+    AuthModule,
+    UserModule,
+    RegionModule,
+    MedicalSpecialtyModule,
+  ],
   controllers: [],
   providers: [],
 })
@@ -16,6 +29,14 @@ export class AppModule implements NestModule {
       .exclude(
         "/auth/register",
         "/auth/login",
+        {
+          path: "/region",
+          method: RequestMethod.GET,
+        },
+        {
+          path: "/medical-specialty",
+          method: RequestMethod.GET,
+        },
       )
       .forRoutes("*");
   }
