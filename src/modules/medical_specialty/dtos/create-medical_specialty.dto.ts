6f878c0c4fa5c644077fa521214da4dbe9d8e9a6
Fabricio Lima <fabriciotrab2005@gmail.com>
2024-02-22 02:38:25 -0300

    feat: dto create medical specialty

--- src/modules/medical_specialty/dtos/create-medical_specialty.dto.ts
+++ src/modules/medical_specialty/dtos/create-medical_specialty.dto.ts
@@ -1,1 +1,7 @@
-export class CreateMedicalSpecialtyDto {}
+import { IsNotEmpty } from "class-validator";
+import { MedicalSpecialty } from "../entities/medical_specialty.entity";
+
+export class CreateMedicalSpecialtyDto extends MedicalSpecialty {
+  @IsNotEmpty()
+  label: string;
+}
